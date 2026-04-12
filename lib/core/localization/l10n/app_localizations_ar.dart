06c6541ff1d4da88da9410e94298d00abcd42315
nada <nadamoafia66@gmail.com>
2026-04-12 12:57:25 +0200

    feat: setup app localization and add language files

---
+++ lib/core/localization/l10n/app_localizations_ar.dart
@@ -0,0 +1,49 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Arabic (`ar`).
+class AppLocalizationsAr extends AppLocalizations {
+  AppLocalizationsAr([String locale = 'ar']) : super(locale);
+
+  @override
+  String get signIn => 'تسجيل الدخول';
+
+  @override
+  String get welcomBack => 'مرحباً بعودتك';
+
+  @override
+  String get emailAddress => 'البريد الإلكتروني';
+
+  @override
+  String get password => 'كلمة المرور';
+
+  @override
+  String get enterPassword => 'أدخل كلمة المرور';
+
+  @override
+  String get forgetPassword => 'نسيت كلمة المرور؟';
+
+  @override
+  String get newToAdaptiveComms => 'جديد في Adaptive Comms?';
+
+  @override
+  String get createAnAccount => 'إنشاء حساب';
+
+  @override
+  String get or => 'او';
+
+  @override
+  String get name => 'الاسم بالكامل';
+
+  @override
+  String get confirmPassword => 'تأكيد كلمة المرور';
+
+  @override
+  String get signUp => 'تسجيل حساب';
+
+  @override
+  String get alreadyHaveAnAccount => 'لديك حساب بالفعل؟';
+}
---
+++ lib/core/localization/l10n/app_localizations_en.dart
@@ -0,0 +1,49 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for English (`en`).
+class AppLocalizationsEn extends AppLocalizations {
+  AppLocalizationsEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get signIn => 'Sign In';
+
+  @override
+  String get welcomBack => 'Welcome Back';
+
+  @override
+  String get emailAddress => 'Email Address';
+
+  @override
+  String get password => 'Password';
+
+  @override
+  String get enterPassword => 'Enter Password';
+
+  @override
+  String get forgetPassword => 'Forget Password?';
+
+  @override
+  String get newToAdaptiveComms => 'New To Adaptive Comms?';
+
+  @override
+  String get createAnAccount => 'Create an Account';
+
+  @override
+  String get or => 'OR';
+
+  @override
+  String get name => 'Full Name';
+
+  @override
+  String get confirmPassword => 'Confirm Password';
+
+  @override
+  String get signUp => 'Sign Up';
+
+  @override
+  String get alreadyHaveAnAccount => 'Already have an account?';
+}
